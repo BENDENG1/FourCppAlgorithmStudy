4eea9dd91a90b23253ae4ceece2d17950a833b3e
GitHubSeob <githubseob@gmail.com>
2023-07-06 20:24:18 +0900

    7월 1주차_3

---
+++ 2023_07/WEEK1/김진섭_점프와 순간 이동.cpp
@@ -0,0 +1,19 @@
+﻿//문제풀이: https://githubseob.tistory.com/264
+
+using namespace std;
+
+int solution(int n)
+{
+    int cnt(0);
+    while (n > 0) {
+        if (n % 2 == 1) {
+            n -= 1;
+            cnt++;
+        }
+        if (n % 2 == 0) {
+            n /= 2;
+        }
+    }
+
+    return cnt;
+}
---
+++ 2023_07/WEEK1/김진섭_카펫.cpp
@@ -0,0 +1,17 @@
+﻿//문제풀이: https://githubseob.tistory.com/196
+
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    int idx(0), idx2(0), quotient(0);
+    for (idx = 1; idx * idx <= yellow; ++idx) {
+        if (yellow % idx == 0) {
+            quotient = yellow / idx;
+            if ((quotient + 2) * (idx + 2) == brown + yellow) {
+                return { quotient + 2, idx + 2 };
+            }
+        }
+    }
+}
