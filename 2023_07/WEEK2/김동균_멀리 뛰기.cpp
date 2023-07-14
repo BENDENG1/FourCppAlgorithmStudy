a70d9311058413e7eee3425c90b1c1a9938d8bc0
user <user@DESKTOP-9JU82II>
2023-07-14 19:52:29 +0900

    7월 2주차

--- 2023_07/WEEK2/김동균_멀리 뛰기.cpp
+++ 2023_07/WEEK2/김동균_멀리 뛰기.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-long long solution(int n) {
+long long solution(int n)
+{
     if (n <= 2)
         return n;
 
@@ -11,9 +12,10 @@ long long solution(int n) {
     dp[1] = 1;
     dp[2] = 2;
 
-    for (int i = 3; i <= n; i++) {
-        dp[i] = (dp[i - 1])%1234567 + (dp[i - 2])%1234567;
+    for (int i = 3; i <= n; i++)
+    {
+        dp[i] = (dp[i - 1]) % 1234567 + (dp[i - 2]) % 1234567;
     }
 
-    return dp[n]%1234567;
+    return dp[n] % 1234567;
 }
