8544f44736e3b37291346865b07faa0b95900984
beeseo147 <87863897+beeseo147@users.noreply.github.com>
2023-09-01 17:22:41 +0900

    "뒤에 큰 수찾기 수정"

--- 2023_08/WEEK4/김동균_뒤에 있는 큰 수 찾기.cpp
+++ 2023_08/WEEK4/김동균_뒤에 있는 큰 수 찾기.cpp
@@ -3,25 +3,33 @@
 
 using namespace std;
 
-vector<int> solution(vector<int> numbers) {
+vector<int> solution(vector<int> numbers)
+{
     vector<int> answer(numbers.size(), -1);
 
-    for (int i = 0; i < numbers.size(); i++) {
-        for (int j = i + 1; j < numbers.size(); j++) {
-            if (numbers[j] > numbers[i]) {
+    for (int i = numbers.size() - 2; i >= 0; i--)
+    {
+        for (int j = i + 1; j < numbers.size(); j++)
+        {
+            if (numbers[i] < numbers[j])
+            {
                 answer[i] = numbers[j];
                 break;
-            } else if (numbers[i] >= numbers[j]) {
-                if (answer[j] == -1) {
+            }
+            else if (numbers[i] >= numbers[j])
+            {
+                if (answer[j] == -1)
+                {
                     answer[i] = -1;
                     break;
-                } else if (numbers[i] < answer[j]) {
+                }
+                else if (numbers[i] < answer[j])
+                {
                     answer[i] = answer[j];
                     break;
                 }
             }
         }
     }
-
     return answer;
 }
