f0dba3d45e7701f5e6450a851eff5e12322b9bf9
beeseo147 <87863897+beeseo147@users.noreply.github.com>
2023-09-04 19:15:25 +0900

    "9월 1주차"

---
+++ 2023_09/김동균_2 x n 타일링.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+
+    vector<int> v;
+
+    v.push_back(1);
+    v.push_back(2);
+
+    for(int i =2;i<n;i++){
+        v.push_back((v[i-1] + v[i-2])%1000000007);
+    }
+    return v[n-1];
+}
