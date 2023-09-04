7fcb05135dbe9b96964e825bce73d1e6dbf6369b
GitHubSeob <githubseob@gmail.com>
2023-09-04 22:26:42 +0900

    9월 1주차_1

---
+++ 2023_09/WEEK1/김진섭_2 x n 타일링.cpp
@@ -0,0 +1,18 @@
+﻿//문제풀이: https://githubseob.tistory.com/328
+
+#include <string>
+#include <vector>
+#define MOD 1000000007
+using namespace std;
+
+int solution(int n) {
+    vector<int>DP(n + 1, 0);
+
+    DP[0] = 1;
+    DP[1] = 1;
+
+    for (int idx = 2; idx <= n; ++idx)
+        DP[idx] = (DP[idx - 2] + DP[idx - 1]) % MOD;
+
+    return DP[n];
+}
---
+++ 2023_09/WEEK1/김진섭_롤케이크 자르기.cpp
@@ -0,0 +1,25 @@
+﻿//문제풀이: https://githubseob.tistory.com/326
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<int> topping) {
+    int answer(0);
+    unordered_map<int, int>types;
+    unordered_map<int, int>older;
+
+    for (int idx = 0; idx < topping.size(); ++idx)
+        ++types[topping[idx]];
+
+    for (int mid = 0; mid < topping.size(); ++mid) {
+        ++older[topping[mid]];
+        --types[topping[mid]];
+        if (types[topping[mid]] == 0) types.erase(topping[mid]);
+        if (older.size() == types.size()) ++answer;
+    }
+
+    return answer;
+}
