902e22509444a2771859920a2f3687e17d246017
KJS <83795383+GitHubSeob@users.noreply.github.com>
2023-09-08 06:09:31 +0900

    9월 1주차_2
    Update

--- 2023_09/WEEK1/김진섭_택배상자.cpp
+++ 2023_09/WEEK1/김진섭_택배상자.cpp
@@ -1,4 +1,4 @@
-﻿//문제풀이: https://githubseob.tistory.com/332
+//문제풀이: https://githubseob.tistory.com/332
 
 #include <string>
 #include <vector>
@@ -8,11 +8,9 @@ using namespace std;
 int solution(vector<int> order) {
     int N = order.size();
     int order_idx(0), answer(0), need(0);
-    vector<bool>is_subConv(N + 1, false);
     stack<int>stk;
 
     int box_num = 1;
-    
     while (1) {
         if (order_idx >= N) break;
         need = order[order_idx];
@@ -25,11 +23,9 @@ int solution(vector<int> order) {
         else if (!stk.empty() && stk.top() == need) {
             ++order_idx;
             ++answer;
-            is_subConv[need] = false;
             stk.pop();
         }
         else {
-            is_subConv[box_num] = true;
             stk.push(box_num++);
         }
     }
