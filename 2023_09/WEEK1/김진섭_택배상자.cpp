7ca31d42bf300409be6c1d63826267354a2cdbfb
GitHubSeob <githubseob@gmail.com>
2023-09-08 06:00:26 +0900

    9월 1주차_2

---
+++ 2023_09/WEEK1/김진섭_택배상자.cpp
@@ -0,0 +1,38 @@
+﻿//문제풀이: https://githubseob.tistory.com/332
+
+#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int solution(vector<int> order) {
+    int N = order.size();
+    int order_idx(0), answer(0), need(0);
+    vector<bool>is_subConv(N + 1, false);
+    stack<int>stk;
+
+    int box_num = 1;
+    
+    while (1) {
+        if (order_idx >= N) break;
+        need = order[order_idx];
+        if (box_num > N && !stk.empty() && stk.top() != need) break;
+        if (need == box_num) {
+            ++order_idx;
+            ++box_num;
+            ++answer;
+        }
+        else if (!stk.empty() && stk.top() == need) {
+            ++order_idx;
+            ++answer;
+            is_subConv[need] = false;
+            stk.pop();
+        }
+        else {
+            is_subConv[box_num] = true;
+            stk.push(box_num++);
+        }
+    }
+
+    return answer;
+}
