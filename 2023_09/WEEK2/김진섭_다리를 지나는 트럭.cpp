15bdf26a8622bc7b3c4486e646699e1968531055
GitHubSeob <githubseob@gmail.com>
2023-09-11 06:37:32 +0900

    9월 2주차_1

---
+++ 2023_09/WEEK2/김진섭_다리를 지나는 트럭.cpp
@@ -0,0 +1,27 @@
+﻿//문제풀이: https://githubseob.tistory.com/336
+
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(int br_len, int max_weight, vector<int> trucks) {
+    int max_cnt(br_len), truck_cnt(0), weight(0), front_truck(0);
+    int time(1), prev_time(0);
+    queue<int>in_time;
+
+    for (int idx = 0; idx < trucks.size(); ++idx) {
+        while (truck_cnt >= max_cnt || weight + trucks[idx] > max_weight) {
+            time = in_time.front() + br_len;
+            in_time.pop();
+            weight -= trucks[front_truck++];
+            --truck_cnt;
+        }
+        if (prev_time >= time) time = prev_time + 1;
+        if (idx + 1 == trucks.size()) return time + br_len;
+        in_time.push(time);
+        ++truck_cnt;
+        weight += trucks[idx];
+        prev_time = time;
+    }
+}
