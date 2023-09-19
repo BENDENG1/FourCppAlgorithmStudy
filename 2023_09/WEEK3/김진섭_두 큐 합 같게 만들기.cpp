c6142816a5f37165793cc44bf5c59d94b0adb163
GitHubSeob <githubseob@gmail.com>
2023-09-19 15:06:05 +0900

    9월 3주차_2

---
+++ 2023_09/WEEK3/김진섭_두 큐 합 같게 만들기.cpp
@@ -0,0 +1,59 @@
+﻿//문제풀이: https://githubseob.tistory.com/342
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#define ll long long
+#define INF 1e7
+using namespace std;
+
+int solution(vector<int> queue1, vector<int> queue2) {
+    int answer(INF);
+    ll sum(0), target(0);
+    vector<int>elements(queue1.size() + queue2.size(), 0);
+
+    for (int idx = 0; idx < queue1.size(); ++idx) {
+        elements[idx] = queue1[idx];
+        target += elements[idx];
+    }
+    for (int idx = queue1.size(); idx < queue1.size() + queue2.size(); ++idx) {
+        elements[idx] = queue2[idx - queue1.size()];
+        target += elements[idx];
+    }
+
+    target /= 2;
+
+    int left(0), right(0);
+    int q1_size(queue1.size()), q2_size(queue2.size());
+
+    while (right < elements.size()) {
+        if (sum < target) {
+            sum += elements[right++];
+        }
+        else if (sum == target) {
+            if (left < queue1.size() && right < queue1.size()) {
+                answer = min(answer, right + q2_size + left);
+            }
+            else if (left < queue1.size() && right >= queue1.size()) {
+                answer = min(answer, left + right - q1_size);
+            }
+            else if (left > queue1.size()) {
+                answer = min(answer, right - q1_size + left);
+            }
+            sum -= elements[left++];
+        }
+        else if (sum > target) {
+            sum -= elements[left++];
+        }
+    }
+
+    while (left < elements.size()) {
+        sum -= elements[left++];
+        if (sum == target) {
+            answer = min(answer, left - q2_size);
+        }
+    }
+
+    if (answer == INF) answer = -1;
+    return answer;
+}
---
+++ 2023_09/WEEK3/김진섭_연속된 부분 수열의 합.cpp
@@ -0,0 +1,41 @@
+﻿//문제풀이: https://githubseob.tistory.com/343
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> sequence, int k) {
+    vector<int>answer(2, 0);
+    answer[1] = sequence.size() - 1;
+    int left(0), right(0), sum(0);
+
+    while (right < sequence.size()) {
+        if (sum < k) {
+            sum += sequence[right++];
+            continue;
+        }
+        else if (sum == k) {
+            if (answer[1] - answer[0] + 1 > right - 1 - left + 1) {
+                answer[1] = right - 1;
+                answer[0] = left;
+            }
+            sum -= sequence[left++];
+        }
+        else if (sum > k) {
+            sum -= sequence[left++];
+        }
+    }
+
+    while (left < sequence.size()) {
+        sum -= sequence[left++];
+        if (sum == k) {
+            if (answer[1] - answer[0] + 1 > right - 1 - left + 1) {
+                answer[1] = right - 1;
+                answer[0] = left;
+            }
+        }
+    }
+
+    return answer;
+}
