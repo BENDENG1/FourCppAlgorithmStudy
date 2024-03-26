fd79dd2e4af179d2858972b328ed3e09772c682c
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-03-26 22:28:30 +0900

    노균욱_백준_책페이지(내일 이어서)

---
+++ 2024_03/WEEK4/노균욱_백준_책페이지.cpp
@@ -0,0 +1,20 @@
+/*
+책 페이지
+
+전체페이지 n => 1~n페이지 
+
+0,1,2...9까지 총 몇번 나오는지 공백으로 출력
+
+*/
+#include <iostream>
+
+int arr[10] = {0,};
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    
+
+}
